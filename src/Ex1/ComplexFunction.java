d2cb0b588568dc49f84211fa6d18967570334e8d
omerugi <omerugi@users.noreply.github.com>
2019-12-04 14:17:39 +0200

    CF constracturs + f + copy + op func

--- src/Ex1/ComplexFunction.java
+++ src/Ex1/ComplexFunction.java
@@ -5,28 +5,108 @@ public class ComplexFunction implements complex_function {
 	function left, right;
 	Operation op;
 	
-	public ComplexFunction() {}
+	
+	public ComplexFunction(function p1) {
+		
+		this.op = Operation.None;
+		this.left = p1;
+		
+	}
+	
+	public ComplexFunction(String op, function left, function right) {
+		
+		switch (op) {
+		case "plus":
+		case "Plus":
+			ComplexFunction cf = new ComplexFunction(Operation.Plus, left,right);
+			this.left = cf.left;
+			this.right = cf.right;
+			this.op = cf.op;
+			break;
+			
+		case "times":
+		case "mult":
+		case "mul":
+		case "Times":
+			ComplexFunction cf1 = new ComplexFunction(Operation.Times, left,right);
+			this.left = cf1.left;
+			this.right = cf1.right;
+			this.op = cf1.op;
+			break;
+		
+			
+		case "divid":
+		case "div":
+		case "Divid":
+			ComplexFunction cf2 = new ComplexFunction(Operation.Divid, left,right);
+			this.left = cf2.left;
+			this.right = cf2.right;
+			this.op = cf2.op;
+			break;
+			
+			
+		case "max":
+		case "Max":
+			ComplexFunction cf3 = new ComplexFunction(Operation.Max, left,right);
+			this.left = cf3.left;
+			this.right = cf3.right;
+			this.op = cf3.op;
+			break;
+			
+		case "min":
+		case "Min":
+			ComplexFunction cf4 = new ComplexFunction(Operation.Min, left,right);
+			this.left = cf4.left;
+			this.right = cf4.right;
+			this.op = cf4.op;
+			break;
+			
+		case "none":
+		case "None":
+			ComplexFunction cf5 = new ComplexFunction(Operation.None, left,right);
+			this.left = cf5.left;
+			this.right = cf5.right;
+			this.op = cf5.op;
+			break;
+			
+		case "comp":
+		case "Comp":
+			ComplexFunction cf6 = new ComplexFunction(Operation.None, left,right);
+			this.left = cf6.left;
+			this.right = cf6.right;
+			this.op = cf6.op;
+			break;
+			
+
+		default:
+			throw new RuntimeException("Not a valid op");
+		}
+		
+	}
 	
 	public ComplexFunction(Operation op, function left, function right) {
 			
 		if(op == Operation.None) {
 			
 			if(right ==null && left==null) {
-				return;
+				throw new RuntimeException("No left function");
 			}
 			if(right == null && left != null) {
 				this.left = left;
 				return;
 			}
 			if(left == null && right != null) {
-				this.left = right;
-				return;
+				throw new RuntimeException("No left function");
 			}
 			if(left != null && right != null) {
-				throw new RuntimeException("Not a valid in put");
+				throw new RuntimeException("Non op");
 			}
 		}
 		else {
+			if(right == null || left == null) {
+				throw new RuntimeException("Null function");
+			}
+					
 			if(op == Operation.Divid) {
 				if(right.toString() == "0") {
 					throw new RuntimeException("Div by 0");
@@ -46,78 +126,149 @@ public class ComplexFunction implements complex_function {
 	
 	@Override
 	public double f(double x) {
-		// TODO Auto-generated method stub
-		return 0;
+		double ans =0;
+	/*	
+		if(this.right == null) {
+			ans=left.f(x);
+		}
+		else
+	*/	
+		ans=op_func(x);
+		return ans;
+	}
+
+	private double op_func(double x) {
+		
+		double ans =0;
+		switch (op) {
+		case Plus:
+			ans = left.f(x)+right.f(x);
+			break;
+		
+		case Times:
+			ans = left.f(x)*right.f(x);
+			break;	
+				
+		case Divid:
+			if ( right.f(x) == 0 ) { 
+				throw new RuntimeException("Div by 0");
+			}
+			ans = left.f(x)/right.f(x);
+			break;
+			
+		case Max:
+			ans = Math.max(left.f(x),right.f(x));
+			break;	
+				
+		case Min:
+			ans = Math.min(left.f(x),right.f(x));
+			break;
+			
+		case Comp:
+			ans = left.f(right.f(x));
+			break;
+		
+		case None:
+			ans = left.f(x);
+			break;
+			
+		default:
+			break;
+		}
+		
+		
+		return ans;
 	}
 
 	@Override
 	public function initFromString(String s) {
 
-		
 		return null;
 	}
 
 	@Override
 	public function copy() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		function copy_obj = new ComplexFunction(this.op,this.left,this.right);
+		return copy_obj;
 	}
 
 	@Override
 	public void plus(function f1) {
-		// TODO Auto-generated method stub
+		
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Plus;
 		
 	}
 
 	@Override
 	public void mul(function f1) {
-		// TODO Auto-generated method stub
-		
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Times;
 	}
 
 	@Override
 	public void div(function f1) {
-		// TODO Auto-generated method stub
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Divid;
 		
 	}
 
 	@Override
 	public void max(function f1) {
-		// TODO Auto-generated method stub
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Max;
 		
 	}
 
 	@Override
 	public void min(function f1) {
-		// TODO Auto-generated method stub
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Min;
 		
 	}
 
 	@Override
 	public void comp(function f1) {
-		// TODO Auto-generated method stub
+		function new_left = this.copy();
+		this.left = new_left;
+		this.right = f1;
+		this.op = Operation.Comp;
 		
 	}
 
 	@Override
-	public function left() {
-		// TODO Auto-generated method stub
-		return null;
+	public function left() {	
+		return this.left;
 	}
 
 	@Override
 	public function right() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.right;
 	}
 
 	@Override
 	public Operation getOp() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.op;
 	}
 	
 	public String toString() {
+		if(left == null) {
+			return op+"("+"0"+","+right.toString()+")";
+		}
+		if(right == null) {
+			return op+"("+left.toString()+","+"0"+")";
+		}
 		return op+"("+left.toString()+","+right.toString()+")";
 	}
 
--- src/Ex1Testing/ComplexFunctionTest.java
+++ src/Ex1Testing/ComplexFunctionTest.java
@@ -10,24 +10,51 @@ import org.junit.jupiter.api.Test;
 class ComplexFunctionTest {
 
 	@Test
-	void testComplexFunction() {
-		fail("Not yet implemented");
+	void testComplexFunctionFunction() {
+		
+		try {
+		String[] s3 = {"x+3","x-2", "x-4"};
+		Polynom p3 = new Polynom(s3[0]);
+		ComplexFunction cf3 = new ComplexFunction(p3);
+		System.out.println(cf3);
+		}catch (Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+			fail("Not yet implemented");
+		}
+
 	}
 
 	@Test
 	void testComplexFunctionOperationFunctionFunction() {
-		
 		try {
-			Polynom p1 = new Polynom("x^2+22+11x");
+			Polynom p = new Polynom("x^2+22+11x");
 			Monom m1 = new Monom("x^3");
-			ComplexFunction test = new ComplexFunction(Operation.Plus, p1, m1);
+			ComplexFunction test = new ComplexFunction(Operation.Plus, p, m1);
 			System.out.println(test);
-			
 			function f1 = new Monom("x^33");
 			ComplexFunction test2 = new ComplexFunction(Operation.Divid, test,f1);
 			System.out.println(test2);
 			
+			
+			String s1 = "3.1+2.4x^2-x^4";
+			String s2 = "5 +2x-3.3x+0.1x^5";
+			String[] s3 = {"x+3","x-2", "x-4"};
+			Polynom p1 = new Polynom(s1);
+			Polynom p2 = new Polynom(s2);
+			Polynom p3 = new Polynom(s3[0]);
+			ComplexFunction cf3 = new ComplexFunction(p3);
+			System.out.println(cf3);
+			for(int i=1;i<s3.length;i++) {
+				cf3.mul(new Polynom(s3[i]));
+			}
+			
+			ComplexFunction cf = new ComplexFunction("Plus", p1, p2);
+			ComplexFunction cf4 = new ComplexFunction("div", new Polynom("x+1"), cf3);
+			
+			
 		}catch (Exception e) {
+			e.printStackTrace();
 			fail("Not yet implemented");
 		}
 		
@@ -36,67 +63,67 @@ class ComplexFunctionTest {
 
 	@Test
 	void testF() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testInitFromString() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testCopy() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testPlus() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testMul() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testDiv() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testMax() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testMin() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testComp() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testLeft() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testRight() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testGetOp() {
-		fail("Not yet implemented");
+		
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+		
 	}
 
 }
