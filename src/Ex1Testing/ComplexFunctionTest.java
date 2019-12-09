267f2279e947e85dd0e757280a01bf5563be0c0d
omerugi <omerugi@users.noreply.github.com>
2019-12-09 22:09:04 +0200

    Monom Junit

--- src/Ex1Testing/ComplexFunctionTest.java
+++ src/Ex1Testing/ComplexFunctionTest.java
@@ -16,8 +16,6 @@ import org.junit.jupiter.api.Test;
 class ComplexFunctionTest {
 
 
-	ComplexFunction cf0,cf1,cf2,cf3,cf4,cf5;
-	ComplexFunction [] cf;
 	ComplexFunction [] complist;
 	Polynom [] polylist;
 	Monom [] monolist;
@@ -66,29 +64,15 @@ class ComplexFunctionTest {
 			complist[i].plus(monolist[random]);
 		}
 		
-		
-		
-		cf=new ComplexFunction[5];
-		cf0=new ComplexFunction("comp(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x +5.0)");
-		cf1=new ComplexFunction("div(plus(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x+5.0),-1.0x^4 +2.4x^2 +3.1)");
-		cf2=new ComplexFunction("mul(2x^2+4+5x^3,1)");
-		cf3=new ComplexFunction("-1.0x^4 +2.4x^2 +3.1");
-		cf4=new ComplexFunction("max(min(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x+5.0),-1.0x^4 +2.4x^2 +3.1)");
-		cf[0]=cf0;
-		cf[1]=cf1;
-		cf[2]=cf2;
-		cf[3]=cf3;
-		cf[4]=cf4;
 	}
 	
 	@AfterEach
 	void tearDown() throws Exception
 	{
-		for(int i=0;i<cf.length;i++) {
+		for(int i=0;i<monolist.length;i++) {
 			polylist[i]=null;
 			monolist[i]=null;
 			str[i]=null;
-			cf[i]=null;
 		}
 			
 	}
@@ -353,7 +337,7 @@ class ComplexFunctionTest {
 		
 		
 		ComplexFunction [] test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Plus,polylist[random1],polylist[random2]);
@@ -402,7 +386,7 @@ class ComplexFunctionTest {
 	void testMul() {
 
 		ComplexFunction [] test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Times,monolist[random1],monolist[random2]);
@@ -416,7 +400,7 @@ class ComplexFunctionTest {
 		
 		
 		test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Times,polylist[random1],polylist[random2]);
@@ -429,7 +413,7 @@ class ComplexFunctionTest {
 		}
 		
 		test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Times,complist[random1],complist[random2]);
@@ -478,7 +462,7 @@ class ComplexFunctionTest {
 		
 		
 		ComplexFunction [] test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Divid,monolist[random1],monolist[random2]);
@@ -493,7 +477,7 @@ class ComplexFunctionTest {
 		
 		
 		test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Divid,polylist[random1],polylist[random2]);
@@ -507,7 +491,7 @@ class ComplexFunctionTest {
 		}
 		
 		test = new ComplexFunction[5]; 
-		for (int i = 0; i < cf.length; i++) {
+		for (int i = 0; i < test.length; i++) {
 			int random1 = new Random().nextInt(4 + 1);
 			int random2 = new Random().nextInt(4 + 1);
 			test[i] = new ComplexFunction(Operation.Divid,complist[random1],complist[random2]);
@@ -638,12 +622,24 @@ class ComplexFunctionTest {
 	@Test
 	void testComp() {
 		
-		for(int i=1;i<cf.length;i++)
-		{
-			double expected=cf[i].f(cf[i-1].f(i));
-			cf[i].comp(cf[i-1]);
-			double actual=cf[i].f(i);
-			assertEquals(expected,actual,0.00001);
+		ComplexFunction [] test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,polylist[random1],polylist[random2]);
+			double temp1 = test[i].f(3);
+			double temp2 = polylist[random1].f(polylist[random2].f(3));
+			if(temp1 != temp2) {fail();}
+		}
+		
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,monolist[random1],monolist[random2]);
+			double temp1 = test[i].f(3);
+			double temp2 = monolist[random1].f(monolist[random2].f(3));
+			if(temp1 != temp2) {fail();}
 		}
 		
 		Polynom p1 = new Polynom("55.446x");
@@ -673,84 +669,190 @@ class ComplexFunctionTest {
 
 	@Test
 	void testLeft() {
-		function [] actualArr=new ComplexFunction[5];
-		actualArr[0]=new ComplexFunction("-1.0x^4 +2.4x^2 +3.1");
-		actualArr[1]=new ComplexFunction("plus(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x+5.0)");
-		actualArr[2]=new ComplexFunction("2x^2+4+5x^3");
-		actualArr[3]=new ComplexFunction("-1.0x^4 +2.4x^2+3.1");
-		actualArr[4]=new ComplexFunction("min(-1.0x^4 +2.4x^2 +3.1,+0.1x^5 -1.2999999999999998x+5.0)");
-		for(int i=0;i<cf.length;i++) 
-		{
-			assertEquals(cf[i].left(),actualArr[i]);
-		}
+		
+		ComplexFunction [] test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,polylist[random1],polylist[random2]);
+			assertEquals(test[i].left(), polylist[random1]);
+		}		
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,complist[random1],complist[random2]);
+			assertEquals(test[i].left(), complist[random1]);
+		}		
+		
 	}
 
 	@Test
 	void testRight() {
 		
-		function [] actualArr=new ComplexFunction[5];
-		actualArr[0]=new ComplexFunction("+0.1x^5-1.2999999999999998x +5.0");
-		actualArr[1]=new ComplexFunction("-1.0x^4 +2.4x^2 +3.1");
-		actualArr[2]=new ComplexFunction("1");
-		actualArr[3]=null;
-		actualArr[4]=new ComplexFunction("-1.0x^4 +2.4x^2 +3.1");
-		for(int i=0;i<cf.length;i++) 
-		{
-			assertEquals(cf[i].right(),actualArr[i]);
+		ComplexFunction [] test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,polylist[random1],polylist[random2]);
+			assertEquals(test[i].right(), polylist[random2]);
+		}		
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Comp,complist[random1],complist[random2]);
+			assertEquals(test[i].right(), complist[random2]);
+		}	
+		
+		
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(complist[random1]);
+			assertEquals(test[i].right(), null);
 		}
+		
+		
 	}
 
 	@Test
 	void testGetOp() {
 		
-		Operation [] actualArr= {Operation.Comp,Operation.Divid,Operation.Times,Operation.None,Operation.Max};
-		Operation [] expectedArr=new Operation [5];
-		for(int i=0;i<cf.length;i++) 
-		{
-			expectedArr[i]=cf[i].getOp();
+		Operation [] op= new Operation[6];
+		op[0] = Operation.Comp;
+		op[1] = Operation.Divid;
+		op[2] = Operation.Max;
+		op[3] = Operation.Min;
+		op[4] = Operation.Plus;
+		op[5] = Operation.Times;
+		
+		
+		
+		ComplexFunction [] test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			int random3 = new Random().nextInt(5 + 1);
+			test[i] = new ComplexFunction(op[random3],polylist[random1],polylist[random2]);
+			assertEquals(op[random3], test[i].getOp());
+		}		
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			int random3 = new Random().nextInt(5 + 1);
+			test[i] = new ComplexFunction(op[random3],complist[random1],complist[random2]);
+			assertEquals(op[random3], test[i].getOp());
+			
+		}	
+		
+		//test null; 
+		test = new ComplexFunction[5];
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.None,complist[random1],null);
+			assertEquals(Operation.None, test[i].getOp());
+			
 		}
-		assertArrayEquals(expectedArr,actualArr);
-	
+		//test Error; 
+		test = new ComplexFunction[5];
+		
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			assertThrows( RuntimeException.class,() -> new ComplexFunction(Operation.Error,complist[random1],null));
+		}
 	}
 
 	@Test
 	void testToString() {
+		
+		ComplexFunction cf = new ComplexFunction("x");
+		function [] temp = new ComplexFunction[5]; 
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = cf.initFromString(complist[i].toString());
+		}
+		
+		for (int i = 0; i < temp.length; i++) {
+			assertEquals(temp[i].toString(), complist[i].toString());
+		}
 	}
 	
 	@Test
 	void testequals() {
 		
-		ComplexFunction cf0=new ComplexFunction("mul(x,x)");
-		ComplexFunction cf1=new ComplexFunction("mul(x^2,1)");
-		assertTrue(cf1.equals(cf0));
-		ComplexFunction cf2=new ComplexFunction("plus(3x,4x)");
-		ComplexFunction cf3=new ComplexFunction("mul(x,7)");
-		assertTrue(cf3.equals(cf2));
-		ComplexFunction cf4=new ComplexFunction("plus(3x+2,4x)");
-		assertFalse(cf4.equals(cf3));
-		Object p0=new Polynom("7x+2");	
-		assertTrue(cf4.equals(p0));
-		Object m0=new Monom("3x^2");
-		ComplexFunction cf5=new ComplexFunction("3x^2");
-		ComplexFunction cf6=new ComplexFunction("none(3x^2,null)");
-		assertTrue(cf6.equals(m0));
-		assertTrue(cf5.equals(m0));
+		ComplexFunction [] test = new ComplexFunction[5]; 
+		ComplexFunction [] test2 = new ComplexFunction[5]; 
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Plus,monolist[random1],monolist[random2]);
+			test2[i]= new ComplexFunction(Operation.Plus,monolist[random2],monolist[random1]); 
+			if(!(test[i].equals(test2[i]))) {
+				fail(); 
+			}
+				
+		}
 		
-		
-		System.out.println("***testequals:***");
+		test = new ComplexFunction[5]; 
+		test2 = new ComplexFunction[5]; 
+		for (int i = 0; i < test.length; i++) {
+			int random1 = new Random().nextInt(4 + 1);
+			int random2 = new Random().nextInt(4 + 1);
+			test[i] = new ComplexFunction(Operation.Times,polylist[random1],polylist[random2]);
+			test2[i]= new ComplexFunction(Operation.Times,polylist[random2],polylist[random1]); 
+			if(!(test[i].equals(test2[i]))) {
+				fail(); 
+			}
+				
+		}
 		try {
 		Monom m1 = new Monom("x");
 		Polynom p1 = new Polynom("x^2");
 		ComplexFunction cf = new ComplexFunction(Operation.Divid,p1,m1);
-		
-		System.out.println(cf.equals(m1));
+	
+		assertTrue(cf.equals(m1));
 		}catch (Exception e) {
 			System.out.println(e);
 			e.printStackTrace();
 			fail();
 		}
-		System.out.println("****");
+		try {
+			Monom m1 = new Monom("x^2");
+			Polynom p1 = new Polynom("x");
+			ComplexFunction cf = new ComplexFunction(Operation.Times,p1,p1);
+			assertTrue(cf.equals(m1));
+			}catch (Exception e) {
+				System.out.println(e);
+				e.printStackTrace();
+				fail();
+			}
 
+
+			Monom m0 = new Monom("1");
+			Monom m1 = new Monom("12x^2");
+			Monom m2 = new Monom("12x");
+			Monom m3 = new Monom("3");
+			Monom m4 = new Monom("x");
+			
+			
+			Polynom p1 = new Polynom("6x");
+
+			ComplexFunction cf1 = new ComplexFunction(Operation.Times,m1,m1);
+			cf1.div(m1);//==12x^3
+			cf1.div(m4);//==12x suppose to..
+			assertTrue(cf1.equals(m2));
+			ComplexFunction cf2 = new ComplexFunction(Operation.Divid,p1,p1); //==1;
+			
+			assertTrue(cf2.equals(m0)); 
+			
+			cf2.mul(m1);//==12x^2
+			cf2.div(m4);//==12x
+			assertTrue(cf2.equals(m2));
+			
 	}
-
+	
+	
 }
--- src/Ex1Testing/MonomTest.java
+++ src/Ex1Testing/MonomTest.java
@@ -3,6 +3,8 @@ package Ex1Testing;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Random;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.AfterAll;
@@ -13,239 +15,389 @@ import org.junit.jupiter.api.Test;
 
 import Ex1.Monom;
 import Ex1.Polynom;
+import Ex1.Polynom_able;
 
 class MonomTest {
-
-	Monom m0,m1,m2,m3,m4;
-	Monom [] mon;
-
+	
+	Monom [] monolist;
+	String [] str;
+	
 	@BeforeEach
 	public void setUp() throws Exception 
 	{
-		mon=new Monom[5];
-		m0=new Monom(0,0);
-		m1=new Monom(1,1);
-		m2=new Monom(3,0);
-		m3=new Monom(-2.5,2);
-		m4=new Monom(3,4);
-		mon[0]=m0;
-		mon[1]=m1;
-		mon[2]=m2;
-		mon[3]=m3;
-		mon[4]=m4;
+
+		monolist = new Monom[5];
+		str = new String[5];
+		double start = -50;
+		double end = 50;
+		for (int i = 0; i < 5; i++) {
+			double random = new Random().nextDouble();
+			double result = start + (random * (end - start));
+			
+			str[i] = result+"x^";
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			int random = new Random().nextInt(9 + 1)+2;
+			str[i] =str[i]+""+random;
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			monolist[i] = new Monom(str[i]);
+		}
+		
 
 	}
 	@AfterEach
 	public void tearDown() throws Exception 
 	{
-		for(int i=0;i<mon.length;i++) 
+		for(int i=0;i<monolist.length;i++) 
 		{
-			mon[i]=null;
+			monolist[i]=null;
 		}
 	}
 
 	@Test
 	void testMonomDoubleInt() {
 
-		fail("Not yet implemented");
+		double start = -50;
+		double end = 50;
+		double [] coo = new double[5];
+		
+		for (int i = 0; i < 5; i++) {
+			double random = new Random().nextDouble();
+			double result = start + (random * (end - start));
+			coo[i] = result;
+		}
+		int [] power = new int[5];
+		for (int i = 0; i < 5; i++) {
+			int random = new Random().nextInt(9 + 1)+1;
+			power[i] = random;
+		} 
+		for (int i = 0; i < power.length; i++) {
+			Monom m1 = new Monom(coo[i],power[i]);
+		}
+		
 	}
 
 	@Test
 	void testMonomMonom() {
-		for(int i=0;i<mon.length;i++) 
+		
+		for(int i=0;i<monolist.length;i++) 
 		{
-			Monom actual=new Monom(mon[i]);
-			assertEquals(mon[i],actual);
+			Monom temp=new Monom(monolist[i]);
+			assertEquals(monolist[i],temp);
 		}
 	}
 
 	@Test
 	void testGet_coefficient() {
-		double[] expectedArr= {0,1,3,-2.5,3};
-		double[] actualArr= new double[5];
-		for(int i=0;i<mon.length;i++) 
-		{
-			actualArr[i]=mon[i].get_coefficient();
-			assertEquals("testing get coefficent:",expectedArr[i],actualArr[i],0.00001);
+
+		double start = -50;
+		double end = 50;
+		double [] coo = new double[5];
+		
+		for (int i = 0; i < 5; i++) {
+			double random = new Random().nextDouble();
+			double result = start + (random * (end - start));
+			coo[i] = result;
 		}
+		int [] power = new int[5];
+		for (int i = 0; i < 5; i++) {
+			int random = new Random().nextInt(9 + 1)+1;
+			power[i] = random;
+		} 
+		for (int i = 0; i < power.length; i++) {
+			Monom m1 = new Monom(coo[i],power[i]);
+			double temp = coo[i];
+			double temp2 = m1.get_coefficient();
+			if(temp!=temp2) {fail();}
+		}
+		
 	}
 
 	@Test
 	void testGet_power() {
-		int[] expectedArr= {0,1,0,2,4};
-		int[] actualArr= new int[5];
-		for(int i=0;i<mon.length;i++) 
-		{
-			actualArr[i]=mon[i].get_power();
-			assertEquals("testing f:",expectedArr[i],actualArr[i],0.00001);
+		
+		double start = -50;
+		double end = 50;
+		double [] coo = new double[5];
+		
+		for (int i = 0; i < 5; i++) {
+			double random = new Random().nextDouble();
+			double result = start + (random * (end - start));
+			coo[i] = result;
 		}
+		int [] power = new int[5];
+		for (int i = 0; i < 5; i++) {
+			int random = new Random().nextInt(9 + 1)+1;
+			power[i] = random;
+		} 
+		for (int i = 0; i < power.length; i++) {
+			Monom m1 = new Monom(coo[i],power[i]);
+			int temp = power[i];
+			int temp2 = m1.get_power();
+			if(temp!=temp2) {fail();}
+		}
+		
 	}
 
 	@Test
 	void testDerivative() {
-		Monom m1 = new Monom(5,3);
-		Monom m2 =new Monom(2,5);
-		Monom m4 =new Monom(4,5);
 
-
-		Monom dm1 = new Monom(15,2);
-		Monom dm2 = new Monom(10,4);
-		Monom dm4 = new Monom(20,4);
-
-		Monom d1 = (Monom) m1.derivative();
-		Monom d2 = (Monom) m2.derivative();
-		Monom d4 = (Monom) m4.derivative();
-
-		assertEquals(d2.toString(), dm2.toString());
-		assertEquals(d1.toString(), dm1.toString());
-		assertEquals(d4.toString(),dm4.toString());
-
-		Monom[] expectedArr= {new Monom("0"),new Monom("1"),new Monom("0"),new Monom("-5x"),new Monom("12x^3")};
-		Monom[] actualArr=new Monom[5];
-		for(int i=0;i<mon.length;i++) 
-		{
-			actualArr[i]=new Monom(mon[i].derivative());
-		}
-		assertArrayEquals(expectedArr,actualArr);
+		String[] test = 	{"12x^2","6.03x^4","x^8","25x^11"};
+		String[] result = {"24x", "24.12x^3", "8x^7","275x^10"};
+		
+		for (int i = 0; i < result.length; i++) {
+			Monom m = new Monom(test[i]);
+		
+			Monom m1= new Monom(result[i]);
+			assertEquals(m.derivative(),m1);
+			}
 	}
 
 
 	@Test
 	void testF() {
-		Monom m1 = new Monom(5,3);
-		double f1, temp;
-		for (int i = 1; i < 5; i++) {
-			f1=m1.f(i);
-			temp= 5*(Math.pow(i, 3));
-			if(temp != f1) {
-				fail();
+		String[] test =    {"12x^2","6.03x^4","x^8","25x^11"};
+		double [] result = {48,96.48,256,51200};
+		
+		for (int i = 0; i < result.length; i++) {
+			Monom m = new Monom(test[i]);
+			double res= result[i];
+			double res2=m.f(2);
+			if(res2!=res) {fail();};
 			}
-		}
-
-		double[] expectedArr= {0,2,3,-10,48};
-		double[] actualArr= new double[5];
-		for(int i=0;i<mon.length;i++) 
-		{
-			actualArr[i]=mon[i].f(2);
-			assertEquals("testing f:",expectedArr[i],actualArr[i],0.00001);
-		}
 	}
 
 	@Test
 	void testIsZero() {
-		Monom m1 = new Monom(5,3);
-		Monom m2 =new Monom(0,5);
-
-		if(m1.isZero()) {
-			fail("work on your math");
+		
+		Monom [] te = new Monom[5]; 
+		Monom [] tx = new Monom[5]; 
+		for (int i = 0; i < te.length; i++) {
+			te[i]= new Monom (monolist[i]);
+			tx[i]= new Monom (-(monolist[i].get_coefficient()),(monolist[i].get_power()));
+			te[i].add(tx[i]);
+			boolean temp = te[i].isZero();
+			assertTrue(temp);
 		}
-		if(!m2.isZero()) {
-			fail("work on your math");
-		}
-		boolean[] expectedArr= {true,false,false,false,false};
-		boolean[] actualArr= new boolean[5];
-		for(int i=0;i<mon.length;i++) 
-		{
-			actualArr[i]=mon[i].isZero();
-		}
-		assertArrayEquals(expectedArr,actualArr);
+		
 	}
 
 	@Test
 	void testMonomString() {
-		Monom[] actualArr= {new Monom("0"),new Monom("x"),new Monom("3"),new Monom("-2.5x^2"),new Monom("3x^4")};
-		assertArrayEquals(mon,actualArr);
-		assertThrows( RuntimeException.class,() -> new Monom("jdk"));
-		assertThrows( RuntimeException.class,() -> new Monom("3x^"));
-		assertThrows( RuntimeException.class,() -> new Monom("3xf"));
-		assertThrows( RuntimeException.class,() -> new Monom("3x^-2"));
+
+		String[] monoms = {"X", "--x","3fx","2+4","4x^x","2^2x","2(x)","2x2","3x^3+2",
+				"4x^(3+2)", "4x^3i","4x^3d","4x^3f","x^-3"};
+
+		for(int i=0;i<monoms.length;i++) { // Invalid tests
+			try {
+			Monom temp = new Monom(monoms[i]);
+			}catch (RuntimeException e) {// if get RuntimeException = works ok
+				
+			}catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		
+		Monom [] test = new Monom[5];
+		for (int i = 0; i < test.length; i++) {
+			test[i] =new Monom(str[i]);
+			assertEquals(test[i], monolist[i]);
+		}
+
 	}
 
 	@Test
 	void testAdd() {
-		Monom dm1 = new Monom(15,4);
-		Monom dm2 = new Monom(5,4);
-		Monom dm4 = new Monom(20,4);
-		dm1.add(dm2);		
-		assertEquals(dm1.toString(), dm4.toString());
+		
+		String[] monoms1 = {"2","x","-x^2", "12.03x^4"};
+		String[] monoms2 = {"3","3x","x^2","-13.0043x^4"};
+		String[] monoms3 = {"4.999999999","3.9999999x","0","-0.9743x^4"};
+		Monom temp;
+		Monom [] monom_added = new Monom[4];
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added[i] = new Monom(monoms1[i]);
+			monom_added[i].add(new Monom(monoms2[i]));
+			assertEquals(new Monom(monoms3[i]), monom_added[i]);
+		}
+		
+		///
+		
+		Polynom_able p1 = new Polynom(), p2 =  new Polynom();
+		String[] monoms4 = {"2", "-x","-3.2x^2","4","-1.5x^2"};
+		String[] monoms5 = {"5", "1.7x","3.2x^2","-3","-1.5x^2"};
+		
+		for(int i=0;i<monoms4.length;i++) {
+			Monom m = new Monom(monoms4[i]);
+			p1.add(m);
+		}
+		for(int i=0;i<monoms5.length;i++) {
+			Monom m = new Monom(monoms5[i]);
+			p2.add(m);
+		}
+		
+		if (!p1.equals(new Polynom(p1.toString()))) {
+			fail();
+			
+		}
+		if (!p1.equals(new Polynom("-4.7x^2-1.0x+6.0"))) {
+			fail();
+			
+		}
 
-		Monom[] expectedArr= {new Monom("x"),new Monom("2x"),new Monom("6"),new Monom("-5x^2"),new Monom("6x^4")};
-		mon[0].add(mon[1]);
-		mon[1].add(mon[1]);
-		mon[2].add(mon[2]);
-		mon[3].add(mon[3]);
-		mon[4].add(mon[4]);
-		assertArrayEquals(expectedArr,mon);
-		assertThrows( RuntimeException.class,() -> mon[1].add(m2));
 	}
 
 	@Test
 	void testMultipyMonom() {
-		Monom m1 = new Monom(4,2);
-		Monom m2 = new Monom(5,2);
-		Monom m4 = new Monom(20,4);
-		m1.multipy(m2);
-		assertEquals(m1.toString(), m4.toString());
+		String[] monoms1 = {"2","x","x^2", "x^4"};
+		String[] monoms2 = {"3","3x","x^2","x^4"};
+		String[] monoms3 = {"5.99999999","3x^2","x^4","x^8"};
+		Monom temp;
+		Monom [] monom_added = new Monom[4];
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added[i] = new Monom(monoms1[i]);
+			monom_added[i].multipy(new Monom(monoms2[i]));
+			assertEquals(new Monom(monoms3[i]), monom_added[i]);
+		}
 		
-		Monom[] expectedArr= {new Monom("0"),new Monom("x^2"),new Monom("9"),new Monom("0"),new Monom("3x^6")};
-		mon[0].multipy(mon[1]);
-		mon[1].multipy(mon[1]);
-		mon[2].multipy(mon[2]);
-		mon[3].multipy(mon[0]);
-		mon[4].multipy(mon[1]);
-		assertArrayEquals(expectedArr,mon);
 	}
 
 	@Test
 	void testMultipyInt() {
+		
+		String[] monoms1 = {"2","x","x^2", "x^4"};
+		int[] monoms2 =	   {3,3,1,4};
+		String[] monoms3 = {"5.99999999","3x","x^2","4x^4"};
+		Monom temp;
+		Monom [] monom_added = new Monom[4];
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added[i] = new Monom(monoms1[i]);
+			monom_added[i].multipy(monoms2[i]);
+			assertEquals(new Monom(monoms3[i]), monom_added[i]);
+		}
+		
+		String[][] polynoms = {{"3x^2","-6x^3","9x","-2"},{"x" , "3x^3"} , 
+								{"-18.0x^6" , "9.0x^5" ,"21.0x^4" ,"-3.0x^3",
+								"9.0x^2", "-2.0x"}};
+		
+		
+		Polynom p1 = new Polynom();
+		Polynom p2 = new Polynom();
+		Polynom p3 = new Polynom();
+		for (int i = 0; i < polynoms[0].length; i++) {
+			Monom temp2 = new Monom(polynoms[0][i]);
+			p1.add(temp2);
+		}		
+		for (int i = 0; i < polynoms[1].length; i++) {
+			Monom temp2 = new Monom(polynoms[1][i]);
+			p2.add(temp2);
+		}
 
-		Monom m1 = new Monom(3,1);
-		Monom m2 = new Monom("x");
-		m2.multipy(3);
-		assertEquals(m1, m2);
+		for (int i = 0; i < polynoms[2].length; i++) {
+			Monom temp2 = new Monom(polynoms[2][i]);
+			p3.add(temp2);
+		}
+		Polynom multiply = new Polynom();
+		multiply = (Polynom) p1.copy();
+		multiply.multiply(p2);
+
 	}
 
 	@Test
 	void testToString() {
-		Monom dm1 = new Monom(15,4);
-		String s="15.0x^4";
-		String d=dm1.toString();
 
-		assertEquals(d, s);
-		
-		assertEquals("0",mon[0].toString());
-		assertEquals("x",mon[1].toString());
-		assertEquals("3.0",mon[2].toString());
-		assertEquals("-2.5x^2",mon[3].toString());
-		assertEquals("3.0x^4",mon[4].toString());
+		String [] mono_str = new String[5];
+		for (int i = 0; i < mono_str.length; i++) {
+			mono_str[i] = str[i];
+		}
+		assertEquals(mono_str[0], monolist[0].toString());
+		assertEquals(mono_str[1], monolist[1].toString());
+		assertEquals(mono_str[2], monolist[2].toString());
+		assertEquals(mono_str[3], monolist[3].toString());
+		assertEquals(mono_str[4], monolist[4].toString());
 	}
 
 	@Test
 	void testEqualsMonom() {
-		Monom m1=new Monom("3x^2");
-		Monom m2=new Monom("3x^2");
-		Monom m3=new Monom("3");
-		Object p=new Polynom("3");
-		assertTrue(m3.equals(p));
-		assertTrue(m1.equals(m2));
-		assertFalse(m1.equals(m3));
+		
+		String[] monoms1 = {"2","x","-x^2", "12.03x^4"};
+		String[] monoms2 = {"3","3x","x^2","-13.0043x^4"};
+		String[] monoms3 = {"4.999999999","3.9999999x","0","-0.9743x^4"};
+		Monom temp;
+		Monom [] monom_added = new Monom[4];
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added[i] = new Monom(monoms1[i]);
+			monom_added[i].add(new Monom(monoms2[i]));
+			temp =new Monom(monoms3[i]);
+			assertTrue(temp.equals(monom_added[i]));
+		}
+		String[] monoms4 = {"2","x","x^2", "x^4"};
+		String[] monoms5 = {"3","3x","x^2","x^4"};
+		String[] monoms6 = {"5.99999999","3x^2","x^4","x^8"};
+		Monom temp1;
+		Monom [] monom_added2 = new Monom[4];
+		
+		
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added2[i] = new Monom(monoms4[i]);
+			monom_added2[i].multipy(new Monom(monoms5[i]));
+			temp =new Monom(monoms6[i]);
+			assertTrue(temp.equals(monom_added2[i]));
+		}
+		
+		
 	}
+		
+	
 
 	@Test
 	void testInitFromString() {
-		String s="15.0x^4";
-		Monom m= new Monom(0,0);
-		Monom m2= new Monom(15,4);
-		Monom f = (Monom) m.initFromString(s);
 
-		assertEquals(f.toString(), m2.toString());
+		Monom [] mono = new Monom[5];
+		String [] strd = new String[5];
+		double start = -50;
+		double end = 50;
+		for (int i = 0; i < 5; i++) {
+			double random = new Random().nextDouble();
+			double result = start + (random * (end - start));
+			
+			strd[i] = result+"x^";
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			int random = new Random().nextInt(9 + 1)+2;
+			strd[i] =strd[i]+""+random;
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			mono[i] = new Monom(strd[i]);
+		}
+		
+		String[] monoms1 = {"2","x","-x^2", "12.03x^4"};
+		Monom temp;
+		Monom [] monom_added = new Monom[4];
+		Monom [] monom_added2 = new Monom[4];
+		for (int i = 0; i < monom_added.length; i++) {
+			monom_added[i] = new Monom(monoms1[i]);
+			monom_added2[i] = new Monom(monoms1[i]);
+			if(!monom_added[i].equals(monom_added2[i])) {fail();}
+		}
+		
 	}
 
 	@Test
 	void testCopy() {
-		Monom m2= new Monom(15,4);
-		Monom m1= (Monom) m2.copy();
-		assertEquals(m2.toString(), m1.toString());
+		
+		Monom [] temp = new Monom[5];
+		for (int i = 0; i < temp.length; i++) {
+			temp[i]=(Monom) monolist[i].copy();
+			
+			if(!temp[i].equals(monolist[i])) {fail();}
+		}
+		
+		
 	}
 
 }
